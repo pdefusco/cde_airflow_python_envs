c347f39cfb40c4b25f7268675352c6c6c28e917a
pdefusco <pauldefusco@gmail.com>
2024-05-14 13:12:16 -0700

    backup

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# cde_airflow_python_envs
+# CDE Airflow Python Environments
+
+Create the CDE Files resource and load a yaml file to it.
+
+```
+%cde resource create --type files --name myYamlFiles
+
+%cde resource upload --name myYamlFiles --local-path resources/sample.yaml
+```
+
+Create a CDE Python resource and activate the environment.
+
+```
+%cde resource create --name airflow_pyth_resource --type airflow-python-env
+
+%cde resource upload --name airflow_pyth_resource --local-path resources/requirements.txt
+```
+
+Create pipeline resource and upload Airflow DAG to it.
+
+```
+%cde resource create --name my_pipeline_resource   
+
+%cde resource upload --name my_pipeline_resource --local-path code/mydag.py
+
+% cde resource describe --name airflow_pyth_resource                                        
+{
+  "name": "airflow_pyth_resource",
+  "type": "airflow-python-env",
+  "status": "ready",
+  "signature": "b8f2df65fd68efbbf6d3074e857446dd685d1adc",
+  "created": "2024-05-14T19:47:05Z",
+  "modified": "2024-05-14T19:49:22Z",
+  "retentionPolicy": "keep_indefinitely",
+  "files": [
+    {
+      "path": "requirements.txt",
+      "signature": "032692485118e8a3a72196114c6efc4e333c4673",
+      "sizeBytes": 7,
+      "created": "2024-05-14T19:49:22Z",
+      "modified": "2024-05-14T19:49:22Z"
+    }
+  ],
+  "pythonEnv": {
+    "pythonVersion": "python3",
+    "type": "airflow-python-env"
+  },
+  "pythonEnvInfo": {
+    "statusReason": "Build completed",
+    "sizeMb": 13,
+    "buildDurationSeconds": 44,
+    "numberOfFiles": 1172
+  }
+}
+```
+
+Activate Python Environment for Airflow Job.
+
+```
+cde airflow activate-pyenv --pyenv-resource-name airflow_pyth_resource
+```
+
+Create a CDE Airflow Job with the necessary dependencies.
+
+```
+%cde job create --name my_pipeline --type airflow --dag-file mydag.py --mount-1-resource my_pipeline_resource --airflow-file-mount-1-resource my_file_resource --pyenv-resource-name airflow_pyth_resource
+
+%cde job run --name my_pipeline
+```
---
+++ code/mydag.py
@@ -0,0 +1,101 @@
+#****************************************************************************
+# (C) Cloudera, Inc. 2020-2024
+#  All rights reserved.
+#
+#  Applicable Open Source License: GNU Affero General Public License v3.0
+#
+#  NOTE: Cloudera open source products are modular software products
+#  made up of hundreds of individual components, each of which was
+#  individually copyrighted.  Each Cloudera open source product is a
+#  collective work under U.S. Copyright Law. Your license to use the
+#  collective work is as provided in your written agreement with
+#  Cloudera.  Used apart from the collective work, this file is
+#  licensed for your use pursuant to the open source license
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute
+#  this code. If you do not have a written agreement with Cloudera nor
+#  with an authorized and properly licensed third party, you do not
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU,
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# #  Author(s): Paul de Fusco
+#***************************************************************************/
+
+from dateutil import parser
+from datetime import datetime, timedelta
+from datetime import timezone
+from airflow import DAG
+from cloudera.cdp.airflow.operators.cde_operator import CDEJobRunOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+import pendulum
+from yaml import load, dump
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+myResourceFile = "/app/mount/sample.yaml"
+username = "pdefusco_051324"
+
+print("Running as Username: ", username)
+
+#DAG instantiation
+default_args = {
+        'owner': 'pauldefusco',
+        'retry_delay': timedelta(seconds=5),
+        'depends_on_past': False,
+        'start_date': datetime(2023,9,20,0),
+        'schedule_interval':'@hourly',
+        'end_date': datetime(2024,9,30,8)
+        }
+
+dag_name = '{}-python-env-dag'.format(username)
+
+python_env_dag = DAG(
+    dag_name,
+    default_args=default_args,
+    catchup=False,
+    is_paused_upon_creation=False
+)
+
+start = DummyOperator(
+    task_id="start",
+    dag=python_env_dag
+)
+
+#Custom Python Method
+def _parse_yaml(document):
+    print(yaml.load(document, Loader=Loader))
+
+parse_yaml = PythonOperator(
+    task_id="parse_yaml",
+    python_callable=_parse_yaml,
+    op_kwargs={'document': myResourceFile},
+    dag=python_env_dag
+)
+
+end = DummyOperator(
+    task_id="end",
+    dag=python_env_dag
+)
+#Execute tasks in the below order
+
+# step6c only executes when both step6a and step6b have completed
+start >> parse_yaml >> end
